b24df6d2be989a32d9624ea4fbe0c5e0a1315c56
ashleyshakir <ashleymshakir@gmail.com>
2023-08-16 13:50:35 -0400

    able to play again without refreshing

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // grab the gameboard div 
 const gameBoard = document.querySelector("#gameboard")
+// grab the yes and no play again choices div
+const choicesDiv = document.querySelector("#play-again-choices")
 // create array of 9 empty strings - these will hold the game squares
 const gameSquares = ["","","","","","","","",""]
 // grab the game info p tag
 const gameInfo = document.querySelector("#game-info")
-// loop through array creating a div with an id number for each index
-gameSquares.forEach((_square, index) => {
-    const gameSqaure = document.createElement("div")
-    gameSqaure.id = index
-    gameSqaure.classList.add("square")
-    gameBoard.appendChild(gameSqaure)
-    gameSqaure.addEventListener("click",play)
-})
-
-// set cross to go first - will change later when implementing who goes first
-let playerTurn = "cross"
-// update playerTurn variable with user's name 
-gameInfo.innerText = `${playerTurn} player's turn`
 // keep track of turn count
 let turnCount = 0
+// set cross to go first - will change later when implementing who goes first
+let playerTurn = "cross"
+function createBoard(){
+    choicesDiv.innerText = ""
+    turnCount = 0
+    // update playerTurn variable with user's name 
+    gameInfo.innerText = `${playerTurn} player's turn`
+    // loop through array creating a div with an id number for each index
+    gameSquares.forEach((_square, index) => {
+        const gameSqaure = document.createElement("div")
+        gameSqaure.id = index
+        gameSqaure.classList.add("square")
+        gameBoard.appendChild(gameSqaure)
+        gameSqaure.addEventListener("click",play)
+    })
+}
+createBoard()
+
 /** 
  * display crosses and circles on gameboard when square clicked
 */
@@ -68,19 +75,36 @@ function checkForWinner(playerTurn){
         playerTurn = "cross"
     }
     // For each array within the winningCombos array, check if the elements in that array satisfy a specific condition using .every method
-    winningCombos.forEach(array => {
+    winningCombos.forEach((array,index) => {
          // If the square with every id in the array has a first child element with a cross class, then set crossWins to true
         let winner = array.every(id => allSquares[id].firstChild?.classList.contains(playerTurn))
         if (winner) {
             gameInfo.innerText = `${playerTurn} Wins!!!`
             allSquares.forEach(square => square.removeEventListener("click", play))
+            setTimeout(resetGame,2000)
            // allSquares.forEach(square => square.replaceWith(square.cloneNode(true))) // this line creates a copy of each square with the same attributes but removes the event listener
             return 
         } 
     })
+    // if the count reach 8 and there is no winner then it must be a tie!
     if (turnCount === 8){
         gameInfo.innerText = "It's a tie!"
         allSquares.forEach(square => square.removeEventListener("click", play))
     }
 
+}
+function resetGame() {
+    gameInfo.innerText = "Wanna play again?"
+    const yes = document.createElement("p")
+    yes.innerText = "Yes"
+    yes.id = "play-again"
+    const no = document.createElement("p")
+    no.innerText = "No"
+    no.id = "play-again"
+    choicesDiv.appendChild(yes)
+    choicesDiv.appendChild(no)
+    yes.addEventListener("click", () => {
+        gameBoard.innerText = ""
+        createBoard()
+    })
 }
